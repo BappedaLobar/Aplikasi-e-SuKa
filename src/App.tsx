0421e085d5fb349552d4b67e787ece4a893043b1
[dyad] <git@dyad.sh>
2025-08-04 12:22:19 +0800

    [dyad] Memindahkan posisi notifikasi selamat datang - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Sonner />
+        <Sonner position="top-right" />
         <BrowserRouter>
           <Routes>
             <Route path="/login" element={<Login />} />
